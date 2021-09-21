a819882a24d7a69cd4ee3c2a3d2eb32b5a285d15
akseltorgerson <akseltorgerson@gmail.com>
2021-09-20 20:49:41 -0500

    hello world complete

---
+++ main.cpp
@@ -0,0 +1,12 @@
+// Aksel Torgerson
+// atorgerson
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  cout << "Hello, user." << endl;
+  cout << "Nice to meet you!" << endl; 
+  return 0;
+}
